ea700099f8d152d53eadf38ca0c012a295c65b72
nestor <nestorjerez17@gmail.com>
2017-01-10 23:29:25 -0430

    resubida

---
+++ foody/www/js/routes.js
@@ -0,0 +1,177 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+
+
+    .state('menu', {
+      url: '/page1',
+      templateUrl: 'templates/menu.html',
+      controller: 'menuCtrl'
+    })
+
+
+
+
+
+    .state('cart', {
+      url: '/page2',
+      templateUrl: 'templates/cart.html',
+      controller: 'cartCtrl'
+    })
+
+
+
+
+
+    .state('checkOut', {
+      url: '/page3',
+      templateUrl: 'templates/checkOut.html',
+      controller: 'checkOutCtrl'
+    })
+
+
+
+
+
+
+
+    .state('login', {
+      url: '/page4',
+      templateUrl: 'templates/login.html',
+      controller: 'loginCtrl',
+
+		resolve:{
+			"check":function($location){
+				if(sessionStorage.getItem('loggedin_id')){ $location.path('/page9');   }
+				else									 {  $location.path('/page4');   }
+			}
+		}
+    })
+
+
+
+
+
+    .state('signup', {
+      url: '/page5',
+      templateUrl: 'templates/signup.html',
+      controller: 'signupCtrl'
+    })
+
+
+
+
+
+    .state('filterBy', {
+      url: '/page6',
+      templateUrl: 'templates/filterBy.html',
+      controller: 'filterByCtrl'
+    })
+
+
+
+
+
+    .state('sortBy', {
+      url: '/page7',
+      templateUrl: 'templates/sortBy.html',
+      controller: 'sortByCtrl'
+    })
+
+
+
+
+
+    .state('payment', {
+      url: '/page8',
+      templateUrl: 'templates/payment.html',
+      controller: 'paymentCtrl'
+    })
+
+
+
+
+
+    .state('profile', {
+      url: '/page9',
+      templateUrl: 'templates/profile.html',
+      controller: 'profileCtrl'
+    })
+
+
+
+
+
+    .state('myOrders', {
+      url: '/page10',
+      templateUrl: 'templates/myOrders.html',
+      controller: 'myOrdersCtrl'
+    })
+
+
+
+
+
+    .state('editProfile', {
+      url: '/page11',
+      templateUrl: 'templates/editProfile.html',
+      controller: 'editProfileCtrl'
+    })
+
+
+
+
+
+    .state('favorates', {
+      url: '/page12',
+      templateUrl: 'templates/favorates.html',
+      controller: 'favoratesCtrl'
+    })
+
+
+
+
+
+    .state('productPage', {
+      url: '/page13',
+      templateUrl: 'templates/productPage.html',
+      controller: 'productPageCtrl'
+    })
+
+    .state('shop1', {
+      url: '/page14',
+      templateUrl: 'templates/shops/shop1.html',
+      controller: 'shop1Ctrl'
+    })
+
+    .state('shop2', {
+      url: '/page15',
+      templateUrl: 'templates/shops/shop2.html',
+      controller: 'shop2Ctrl'
+    })
+
+    .state('shop3', {
+      url: '/page16',
+      templateUrl: 'templates/shops/shop3.html',
+      controller: 'shop3Ctrl'
+    })
+
+    .state('shop4', {
+      url: '/page17',
+      templateUrl: 'templates/shops/shop4.html',
+      controller: 'shop4Ctrl'
+    })
+
+    ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/page1');
+
+});
